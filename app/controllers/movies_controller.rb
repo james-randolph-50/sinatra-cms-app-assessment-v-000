00d8b28278259a6a434ab7a11c09412ec6a1209f
James Randolph <james.randolph2016@gmail.com>
2018-03-30 19:51:32 +0000

    find by slug and post for mo

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,3 +8,19 @@ class MoviesController < ApplicationController
   get '/movies/new' do
     erb :'/movies/new'
   end
+  
+  get '/movies/:slug' do
+    @movie = Movie.find_by_slug(params[:slug])
+    
+    erb :'movies/show'
+  end
+  
+  post '/movies' do
+    @movie = Movie.create(:name => params["Name"])
+    @movie.genre_ids = params[:genres]
+    @movie.save
+    
+    redirect("/movies/#{@movie.slug}")
+  end
+  
+  
